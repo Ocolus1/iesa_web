ef9c37c5a4abea586eeac61c387312a7e2892a70
Cypherspot <awolesiboluwatife@gmail.com>
2021-04-02 00:34:14 +0100

    changed to sqlitedb

--- application.py
+++ application.py
@@ -14,8 +14,9 @@ from sqlalchemy.exc import IntegrityError
 
 # app configuration 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(username='roots',
-                                        password='root', hostname='localhost', databasename='mydbs')
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(username='roots',
+#                                         password='root', hostname='localhost', databasename='mydbs')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///sqlite3.db'
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.update(
